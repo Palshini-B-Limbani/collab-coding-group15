82d9c7e4a819b3eed588949ae89b2c10ab35a030
H-strangeone <hiteshgitman@gmail.com>
2025-09-05 19:56:30 +0530

    added the palindrome function

--- tests/palindrome_test.rs
+++ tests/palindrome_test.rs
@@ -1,1 +1,7 @@
-
+use collab_coding_group15::palindrome as strings;
+#[test]
+fn test_is_palindrome() {
+    assert!(strings::is_palindrome("Racecar"));
+    assert!(strings::is_palindrome("Never odd or even"));
+    assert!(!strings::is_palindrome("hello"));
+}
