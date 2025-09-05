7109c77d0b87e121d84674063f7ecb4d54b1e872
H-strangeone <154013092+H-strangeone@users.noreply.github.com>
2025-09-05 18:39:28 +0530

    Update palindrome_test.rs

--- tests/palindrome_test.rs
+++ tests/palindrome_test.rs
@@ -1,8 +1,1 @@
-use collab_coding_group15::palindrome;
 
-#[test]
-fn test_is_palindrome() {
-    assert!(palindrome::is_palindrome("Racecar"));
-    assert!(palindrome::is_palindrome("Never odd or even"));
-    assert!(!palindrome::is_palindrome("hello"));
-}
