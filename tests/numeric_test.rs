140a40fc587e2aa9f5500818daf94853aac7a79e
H-strangeone <154013092+H-strangeone@users.noreply.github.com>
2025-09-05 21:17:28 +0530

    Update numeric_test.rs

--- tests/numeric_test.rs
+++ tests/numeric_test.rs
@@ -1,11 +1,12 @@
-use collab_coding_group15::numeric as strings;
+use collab_coding_group15::numeric;
 
 #[test]
 fn test_is_numeric() {
-    assert!(strings::is_numeric("12345"));
-    assert!(!strings::is_numeric("12a45"));
-    assert!(!strings::is_numeric(""));
+    assert!(numeric::is_numeric("12345"));
+    assert!(!numeric::is_numeric("12a45"));
+    assert!(!numeric::is_numeric(""));
 }
+
 
 
 
